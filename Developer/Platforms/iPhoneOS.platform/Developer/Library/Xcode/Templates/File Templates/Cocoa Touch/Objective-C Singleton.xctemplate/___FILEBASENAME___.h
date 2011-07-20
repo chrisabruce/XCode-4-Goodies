6642ae271add9bae9f9dbefcdfdc064dbeaa3e9e
Chris Bruce <chrisabruce@gmail.com>
2011-07-20 14:10:24 -0700

    Added xcode 4 singleton template

---
+++ Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/File Templates/Cocoa Touch/Objective-C Singleton.xctemplate/___FILEBASENAME___.h
@@ -0,0 +1,19 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//	File created using Singleton XCode Template by Mugunth Kumar (http://blog.mugunthkumar.com)
+//  More information about this template on the post http://mk.sg/89
+//  Permission granted to do anything, commercial/non-commercial with this file apart from removing the line/URL above
+
+#import <Foundation/Foundation.h>
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : NSObject {
+
+}
+
++ (___FILEBASENAMEASIDENTIFIER___*) sharedInstance;
+
+@end
---
+++ Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/File Templates/Cocoa Touch/Objective-C Singleton.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,81 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//	File created using Singleton XCode Template by Mugunth Kumar (http://blog.mugunthkumar.com)
+//  More information about this template on the post http://mk.sg/89	
+//  Permission granted to do anything, commercial/non-commercial with this file apart from removing the line/URL above
+
+#import "___FILEBASENAME___.h"
+
+static ___FILEBASENAMEASIDENTIFIER___ *_instance;
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+#pragma mark -
+#pragma mark Singleton Methods
+
++ (___FILEBASENAMEASIDENTIFIER___*)sharedInstance
+{
+	@synchronized(self) {
+		
+        if (_instance == nil) {
+			
+            _instance = [[self alloc] init];
+            
+            // Allocate/initialize any member variables of the singleton class here
+            // example
+			//_instance.member = @"";
+        }
+    }
+    return _instance;
+}
+
++ (id)allocWithZone:(NSZone *)zone
+
+{	
+    @synchronized(self) {
+		
+        if (_instance == nil) {
+			
+            _instance = [super allocWithZone:zone];			
+            return _instance;  // assignment and return on first allocation
+        }
+    }
+	
+    return nil; //on subsequent allocation attempts return nil	
+}
+
+
+- (id)copyWithZone:(NSZone *)zone
+{
+    return self;	
+}
+
+- (id)retain
+{	
+    return self;	
+}
+
+- (unsigned)retainCount
+{
+    return UINT_MAX;  //denotes an object that cannot be released
+}
+
+- (void)release
+{
+    //do nothing
+}
+
+- (id)autorelease
+{
+    return self;	
+}
+
+#pragma mark -
+#pragma mark Custom Methods
+
+// Add your custom methods here
+
+@end
